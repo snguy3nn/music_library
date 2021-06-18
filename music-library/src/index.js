e893c3d021670766313f0f02950c7ba7fafb8747
Stephen Nguyen <snguy3nn@gmail.com>
2021-06-18 10:34:41 -0500

    Created a React environment with initial index.js

---
+++ music-library/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const jsxElement = <h1>Music Library</h1>;
+console.log(jsxElement);
+
+ReactDOM.render(jsxElement, document.getElementById('root'));
